73a533d448dfd1698a4c075c6adb130b053c39ca
kaylorchen <kaylor.chen@qq.com>
2025-08-26 10:16:32 +0800

    add diff model

--- hunter_world/launch/simulation.launch.py
+++ hunter_world/launch/simulation.launch.py
@@ -73,7 +73,7 @@ def generate_launch_description():
 
     declare_world_cmd = DeclareLaunchArgument(
         'world',
-        default_value=WorldType.RMUC,
+        default_value=WorldType.RMUL,
         description='Choose <RMUC>, <WAREHOUSE> or <RMUL>'
     )
 
---
+++ restart_robot.sh
@@ -0,0 +1,2 @@
+ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity '{name: robot}'
+ros2 launch hunter_robot bringup_diff.launch.py
